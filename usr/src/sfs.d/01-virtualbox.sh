9bae5be033ea6afea0e16e801ee25ba9e4378150
Lauri <lauri@korc.jp>
2016-08-06 21:55:16 -0400

    add dkms.conf if needed

--- usr/src/sfs.d/01-virtualbox.sh
+++ usr/src/sfs.d/01-virtualbox.sh
@@ -34,8 +34,33 @@ dpkg -s libqt4-opengl | grep -q "^Status.*installed" || "$lbu/scripts/apt-sfs.sh
 
 echo -n "Creating dkms links .. "
 mod_name="$(find  "$DESTDIR/usr/src" -maxdepth 1 -name vboxhost-\* -printf "%f")"
+
 test -z "$mod_name" || {
   mod_ver="${mod_name#vboxhost-}"
+
+  test -s "$DESTDIR/usr/src/$mod_name/dkms.conf" || cat >"$DESTDIR/usr/src/$mod_name/dkms.conf" <<EOF
+PACKAGE_NAME="vboxhost"
+PACKAGE_VERSION="$mod_ver"
+
+AUTOINSTALL=yes
+
+BUILT_MODULE_NAME[0]="vboxdrv"
+BUILT_MODULE_LOCATION[0]="vboxdrv"
+DEST_MODULE_LOCATION[0]="/extra"
+
+BUILT_MODULE_NAME[1]="vboxpci"
+BUILT_MODULE_LOCATION[1]="vboxpci"
+DEST_MODULE_LOCATION[1]="/extra"
+
+BUILT_MODULE_NAME[2]="vboxnetadp"
+BUILT_MODULE_LOCATION[2]="vboxnetadp"
+DEST_MODULE_LOCATION[2]="/extra"
+
+BUILT_MODULE_NAME[3]="vboxnetflt"
+BUILT_MODULE_LOCATION[3]="vboxnetflt"
+DEST_MODULE_LOCATION[3]="/extra"
+EOF
+
   mkdir -p "$DESTDIR/var/lib/dkms/vboxhost/$mod_ver/build"
   ln -s "/usr/src/$mod_name" "$DESTDIR/var/lib/dkms/vboxhost/$mod_ver/source"
 }
