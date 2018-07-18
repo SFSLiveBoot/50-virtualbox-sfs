6edd2e6a24f56cafd3d91ed4b012a6aaa63794e0
Lauri Korts-Pärn <lauri@korc.jp>
2018-07-18 03:42:40 -0400

    update installing extpack if PUEL is accepted

--- usr/src/sfs.d/01-virtualbox.sh
+++ usr/src/sfs.d/01-virtualbox.sh
@@ -32,14 +32,21 @@ echo "Ok."
 
 dpkg -s libqt4-opengl | grep -q "^Status.*installed" || "$lbu/scripts/apt-sfs.sh" "$DESTDIR" libqt4-opengl
 
-if test -n "$ORACLE_EXT_PACK";then
-  echo -n "Installing $ORACLE_EXT_PACK .. "
+if test -n "$ORACLE_PUEL_ACCEPT";then
+  echo "Oracle PUEL accepted, installing extension pack"
+  if test "x$ORACLE_PUEL_ACCEPT" = "xyes";then
+    deb_ver="$(dpkg-deb -I "$deb" control | sed -ne '/^Version: /{s/.* //;s/-.*//;p;q}')"
+    ext_pack="$(dl_file https://download.virtualbox.org/virtualbox/${deb_ver}/Oracle_VM_VirtualBox_Extension_Pack-${deb_ver}.vbox-extpack)"
+  else
+    ext_pack="$ORACLE_PUEL_ACCEPT"
+  fi
+  echo -n "Installing $ext_pack .. "
   ext_dir="$DESTDIR/usr/lib/virtualbox/ExtensionPacks/Oracle_VM_VirtualBox_Extension_Pack"
   mkdir -p "$ext_dir"
-  tar xfz "$ORACLE_EXT_PACK" --no-same-owner -C "$ext_dir"
+  tar xfz "$ext_pack" --no-same-owner --no-same-permission -C "$ext_dir"
   echo "Done."
 else
-  echo "If you accept Oracle Extension Pack PUEL, you can set ORACLE_EXT_PACK env var to downloaded extpack file"
+  echo "If you accept Oracle Extension Pack PUEL, you can set ORACLE_PUEL_ACCEPT env var to 'yes' or to a downloaded extpack file"
 fi
 
 echo "Ok."
